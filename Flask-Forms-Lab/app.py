989d05e82fa12ece5ab97cec3c76f946506ab82c
eitansa23-meet <eitaneitan121@gmail.com>
2022-07-27 14:39:07 +0300

    1

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -8,15 +8,31 @@ app = Flask(  # Create a flask app
 )
 
 
-username = "llo2ay"
+username = "eitan"
 password = "123"
-facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+facebook_friends=["Daniel","Yonathan","Adan", "George", "Fouad", "Celina"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/' , methods=['GET','POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
-  
+	if request.method == 'POST':
+		if username == request.form['username'] and password == request.form['password']:
+			return render_template('home.html')
+		else:
+
+			return render_template('login.html')
+			
+		
+	else:
+		return render_template('login.html')
+
+@app.route('/home' , methods=['GET','POST'])
+def go_home():
+	return render_template('home.html')
+
+			
+
+
 
 
 
